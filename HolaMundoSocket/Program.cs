d7c7af49f98290c2f96f1839c404cff4781d0f88
laboratorio2 <laboratorio2@TEM_LAB0226>
2022-04-11 16:34:03 -0400

    program

--- HolaMundoSocket/Program.cs
+++ HolaMundoSocket/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HolaMundoSocket.Comunicaciones;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,21 @@ namespace HolaMundoSocket
     {
         static void Main(string[] args)
         {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+
+            Console.WriteLine("Iniciando Servidor en puerto {0}", puerto);
+            ServerSocket servidor = new ServerSocket(puerto);
+
+            if (servidor.Iniciar())
+            {
+                //OK puede conectar
+                Console.WriteLine("Servidor Iniciado");
+
+            }
+            else
+            {
+                Console.WriteLine("Errror, el puerto {0} esta en uso", puerto);
+            }
         }
     }
 }
